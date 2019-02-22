ad6f43010564bab98a6a3725d459572d6372d9a0
1codechic <1codechic@devchics.com>
2019-02-21 22:58:17 -0500

    added comments to dictionary_app.rb with for Definition, Top Example, and Pronunctiation HTTP calls

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -4,16 +4,17 @@ require 'http'
 p "Hello, please enter a word to lookup:"
 word = gets.chomp
 
-
+#Get Definition
 definition_response = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=")
  
 p dictionary = definition_response.parse[0]["text"]
 
-
+#Get Top Example
 top_example_response = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/topExample?useCanonical=false&api_key=")
 
 p top_example = top_example_response.parse["text"]
 
+#Get Pronunctiation
 pronunctiation_response = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/pronunciations?useCanonical=false&limit=50&api_key=")
 
 p pronunciation = pronunctiation_response.parse[0]["raw"]
